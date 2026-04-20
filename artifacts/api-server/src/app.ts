5dc608468f56e6ea00248a5bb93e5bf334a39899
Pablo-2026 <pablola12_roy@hotmail.com>
2026-04-20 03:28:22 -0300

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -29,8 +29,8 @@ app.use(
 );
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Redirección de imágenes a Cloudinary
 app.use("/api/uploads", (req, res) => {
